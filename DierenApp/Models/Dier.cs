0d09307f24fdef4abe903a751522fcc603d1468c
Roy Mengelers <roy.mengelers@zuyd.nl>
2024-12-17 13:12:38 +0100

    Added class diagrams

---
+++ DierenApp/Models/Dier.cs
@@ -0,0 +1,22 @@
+﻿namespace DierenApp.Models
+{
+    internal class Dier
+    {
+        public string Naam { get;  set; }
+        public int AantalPoten { get; set; }
+        public string Geluid { get;  set; }
+        public bool HeeftStaart { get;  set; }
+        public Dier()
+        {
+            
+        }
+
+        public Dier(string naam, int aantalPoten, string geluid, bool heeftStaart)
+        {
+            this.Naam = naam;
+            this.Geluid = geluid;
+            this.HeeftStaart = heeftStaart;
+            this.AantalPoten = aantalPoten;
+        }
+    }
+}
--- DierenApp/Program.cs
+++ DierenApp/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace DierenApp
+﻿using DierenApp.Models;
+
+namespace DierenApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dier olifant = new Dier();
+            olifant.Naam = "Hendrik";
+            olifant.AantalPoten = 4;
+            olifant.HeeftStaart = true;
+            olifant.Geluid = "PFFVVOOOEEEWW";
+
+            Console.WriteLine($"{olifant.Naam}, {olifant.AantalPoten}");
+            Console.ReadLine();
+
+            //Dier hond = new Dier { 
+            //     Naam = "Max",
+            //     AantalPoten = 4,
+            //     Geluid = "Woef Woef!",
+            //     HeeftStaart = true,
+            //};
+
+            //Console.WriteLine($"{hond.Naam}, {hond.Geluid}");
+            //Console.ReadLine();
+
+            Dier kat = new Dier("Felix", 4, "Miauw, Miauw!", true);
+
+            Console.WriteLine($"{kat.Naam}, {kat.Geluid}");
+
+            olifant.AantalPoten = 2;
+
+            Console.WriteLine($"{olifant.Naam}, {olifant.AantalPoten}");
+            Console.ReadLine();
+
+            //List<string> kleuren = new List<string>();
+            //kleuren.Add("rood");
         }
     }
 }
